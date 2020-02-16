ac73352f93c89937377f460d09ebd9857cb9f5bd
gen0205 <12490153+gen0205@users.noreply.github.com>
2020-02-16 15:39:19 +0900

    https://ja.reactjs.org/tutorial/tutorial.html#overview まで完了

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    // サブクラスのコンストラクタ生成時は必ずsuper()を呼ぶ
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
@@ -14,7 +25,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
